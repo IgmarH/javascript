7e227979d93a9afce476bccf728b55528e63af5b
IgmarH <35600123+IgmarH@users.noreply.github.com>
2018-09-25 11:40:03 +0200

    Add files via upload

---
+++ vulmetjs.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subkop").innerText = "Mijn geweldige subkop";
+document.getElementById("thunder1").src = "img/thunder1.jpg";
+document.getElementById("thunder2").src = "img/thunder2.jpg";
